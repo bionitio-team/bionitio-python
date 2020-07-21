a911bd5368cf923b41c6a4b003351a69dfd149a3
Phil Ewels <phil.ewels@scilifelab.se>
2020-07-21 10:37:38 +0200

    Spruce up setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-LONG_DESCRIPTION = \
-'''The program reads one or more input FASTA files.
-For each file it computes a variety of statistics, and then
-prints a summary of the statistics as output.
+# Remember to change when making a new release
+version = '0.1.0.0'
 
-The goal is to provide a solid foundation for new bioinformatics command line tools,
-and is an ideal starting place for new projects.'''
-
+with open("README.md") as f:
+    readme = f.read()
 
 setup(
     name='bionitio',
-    version='0.1.0.0',
+    version=version,
+    description='A prototypical bioinformatics command line tool',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    keywords=['bionitio','bioinformatics','fasta'],
     author='BIONITIO_AUTHOR',
     author_email='BIONITIO_EMAIL',
-    packages=['bionitio'],
+    url='https://github.com/BIONITIO_GITHUB_USERNAME/bionitio',
+    license='MIT',
     package_dir={'bionitio': 'bionitio'},
     entry_points={
         'console_scripts': ['bionitio = bionitio.bionitio:main']
     },
-    url='https://github.com/BIONITIO_GITHUB_USERNAME/bionitio',
-    license='LICENSE',
-    description=('A prototypical bioinformatics command line tool'),
-    long_description=(LONG_DESCRIPTION),
     install_requires=["biopython"],
+    setup_requires=["twine>=1.11.0", "setuptools>=38.6."],
+    packages=find_packages(exclude=("readme_includes", "functional_tests")),
 )
